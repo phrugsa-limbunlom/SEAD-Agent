3af2d8bf440f14c247cc5625da9b439e0a74be3a
phrugsa-limbunlom <phrugsa.lim@gmail.com>
2025-07-17 17:05:53 +0100

    remove unnecessary files

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == "python" && "$2" == "/home/src/tests/run_tests.py" ]]; then
-    exec "$@"
-else
-    # Run FastAPI app on port 8080
-    exec uvicorn src.main:app --host 0.0.0.0 --port 8080
-fi
--- tests/run_tests.py
+++
@@ -1,6 +0,0 @@
-def test_dummy():
-    assert 1 + 1 == 2
-
-if __name__ == "__main__":
-    test_dummy()
-    print("✅ All tests passed.")
